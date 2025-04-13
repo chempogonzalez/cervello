8a9cd4a1d344825a748e652652e1891b8e2ffda8
chempogonzalez <manugonzalezfranco@gmail.com>
2025-04-13 11:50:28 +0200

    export new non-reactive function

--- packages/cervello/src/index.ts
+++ packages/cervello/src/index.ts
@@ -1,6 +1,6 @@
 // export { cervello } from './lib/store'
 //
 // export type { CervelloUseFunction as UseFunction, CervelloOptions } from './lib/store'
-export { cervello } from './lib/store/new-index'
+export { cervello, nonReactive } from './lib/store/new-index'
 
 export type { CervelloOptions, CervelloUseStoreOptions } from './lib/store/new-index'
